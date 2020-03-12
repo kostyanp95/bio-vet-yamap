138531846fd4f15b38e84ceb17bcec1c4af1fcd9
Konstantin Puganov <51831069+kostyanp95@users.noreply.github.com>
2020-03-12 22:03:52 +0300

    Залил проект

---
+++ js/without-localhost.js
@@ -0,0 +1,377 @@
+
+var data = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "id": 0,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.880778, 37.587865]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/address/altufyevo'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>Алтуфьевское шоссе дом 56</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 1,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.820759, 37.822828]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=https://bio-vet.ru/address/shhelkovskaya'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>ул. Алтайская улица, 17А</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 2,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.645588, 37.517922]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=https://bio-vet.ru/address/belyayevo-2'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>ул. Миклухо Маклая, дом 18к1</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 3,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.697767, 37.859210]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/address/zhulebino'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>Октябрьский проспект д.5</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 4,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.656809, 37.773071]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/address/marino'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>Перервинский Бульвар д. 21/1</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 5,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.760049, 37.773116]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/addres/perovo'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>1-я Владимирская, 4</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 6,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.691597, 37.721076]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/addres/pechatniki'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>Печатники, ул Гурьянова д.4 к.1</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 7,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.653853, 37.407079]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/address/solntsevo'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>Солнцево, ул. Главмосстроя дом 7</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 8,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.781585, 37.479708]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/address/khoroshevo-mnevniki'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>ул. Народного Ополчения 29к1</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 9,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.617027, 37.742860]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/address/shipilovskaya'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>ул. Мусы Джалиля 38к1</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 10,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.608709, 37.734371]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/address/veterinarnaya-klinika-zyablikovo'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>ул. Елецкая 11 к2</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 11,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.754056, 37.856632]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/address/veterinarnaya-klinika-reutov-bio-vet'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>ул. Калинина, д.12</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 12,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.870415, 37.656182]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/address/veterinarnaya-klinika-babushkinskaya-bio-vet'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>ул. Менжинского 29</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 13,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.724211, 37.571282]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/address/veterinarnaya-klinika-sportivnaya-bio-vet'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>ул.Доватора, д.3</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 14,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.747551, 37.802302]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/address/novogireevo'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>ул. Новогиреевская дом 53</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 15,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.656590, 37.597594]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/address/veterinarnaya-klinika-kaxovskaya-bio-vet'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>ул. Болотниковская, 21с8</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 16,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.790514, 37.795448]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/address/veterinarnaya-klinika-pervomajskaya-bio-vet'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>ул. Нижняя Первомайская, д. 12 A</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 17,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.664096, 37.536517]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/address/veterinarnaya-klinika-kaluzhskaya-bio-vet'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>ул. Воронцовские пруды д. 3</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 18,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.756437, 37.823655]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/veterinarnaya-klinika-sapernyij-bio-vet'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>Саперный проезд д6 к1</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 19,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.569840, 37.569054]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/address/veterinarnaya-klinika-butovo-bio-vet'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>ул. Куликовская, 3Д</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 20,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.543531, 37.524740]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=http://www.bio-vet.ru/address/veterinarnaya-klinika-yuzhnoe-butovo-bio-vet'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>ул. Адмирала Лазарева, д. 35</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 21,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.880833, 37.693552]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=https://bio-vet.ru/address/medvedkovo'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>ул. Малыгина 9с2</p>"
+            }
+        },
+        {
+            "type": "Feature",
+            "id": 22,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [55.880833, 37.693552]
+            },
+            "properties": {
+                "balloonContentHeader": "<font size=3><b><a target='_blank' href=https://bio-vet.ru/address/medvedkovo'>Страница отделения</a></b></font>",
+                "balloonContentBody": "<p>ул. Малыгигна 9с2</p>"
+            }
+        }
+    ]
+};
+
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 10,
+        controls: ['geolocationControl', 'searchControl']
+    });
+
+    var searchControl = myMap.controls.get('searchControl');
+    searchControl.options.set({
+        noPlacemark: true,
+        placeholderContent: 'Введите адрес'
+    });
+    searchControl.events.add('resultshow', function (e) {
+        highlightResult(searchControl.getResultsArray()[e.get('index')]);
+    });
+
+    myMap.controls.get('geolocationControl').events.add('locationchange', function (e) {
+        highlightResult(e.get('geoObjects').get(0));
+    });
+
+    var deliveryPoint = new ymaps.GeoObject({
+        geometry: {
+            type: 'Point'
+        },
+        properties: {
+            iconCaption: 'Адрес'
+        }
+    }, {
+        preset: 'islands#redDotIconWithCaption',
+        draggable: false,
+        iconCaptionMaxWidth: '215'
+    });
+    myMap.geoObjects.add(deliveryPoint);
+
+    function highlightResult(obj) {
+        var coords = obj.geometry.getCoordinates();
+        deliveryPoint.geometry.setCoordinates(coords);
+        buildRoutes(obj);
+    }
+
+    var points = ymaps.geoQuery(data).addToMap(myMap);
+    points.setOptions('preset', 'islands#blueStretchyIcon')
+
+    var route_dests = [];
+
+    function buildRoutes(origin) {
+        clearRoutes(route_dests);
+        var destinations = [];
+        var circle = new ymaps.Circle([origin.geometry.getCoordinates(), 3000]);
+        myMap.geoObjects.add(circle);
+        points.searchInside(circle).each(function (o) {
+            destinations.push(o);
+        });
+        myMap.geoObjects.remove(circle);
+        if (destinations.length === 0) {
+            destinations = [points.getClosestTo(origin)];
+        }
+        destinations.forEach(function (dest) {
+            var multiRoute = new ymaps.multiRouter.MultiRoute({
+                referencePoints: [
+                    origin.geometry.getCoordinates(),
+                    dest.geometry.getCoordinates()
+                ],
+                params: {
+                    results: 1
+                }
+            }, {
+                boundsAutoApply: true,
+                wayPointVisible: false
+            });
+            myMap.geoObjects.add(multiRoute);
+            multiRoute.model.events.add("requestsuccess", function (event) {
+                var route = event.get("target").getRoutes()[0];
+                dest.properties.set({
+                    iconContent: route.properties.get("distance").text + ', ' +
+                        route.properties.get("duration").text
+                });
+            });
+            route_dests.push([multiRoute, dest]);
+        });
+    }
+
+    function clearRoutes(route_dests) {
+        route_dests.forEach(function (route_dest) {
+            myMap.geoObjects.remove(route_dest[0]);
+            route_dest[1].properties.set({
+                iconContent: ''
+            });
+        });
+        route_dests = [];
+    }
+}
